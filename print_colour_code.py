1c47c1f82797e2a513d9bddd29f848eed26e7d52
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-22 00:23:48 +0530

    Update print_colour_code.py

--- print_colour_code.py
+++ print_colour_code.py
@@ -1,9 +1,10 @@
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 import pandas as pd
 
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
 def print_table():
-    data = {'Major colour':['White', 'White', 'White', 'White','White','Red','Red','Red','Red','Red','Black','Black','Black','Black','Black', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Yellow','Violet','Violet','Violet','Violet','Violet','Violet'],
+    data = {'Major colour':['White', 'White', 'White', 'White','White','Red','Red','Red','Red','Red','Black','Black','Black','Black','Black', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Yellow','Violet','Violet','Violet','Violet','Violet'],
         'Minor colour':["Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate"]}
  
     df = pd.DataFrame(data)
