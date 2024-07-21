1e7ded4990478afa4cea9a469e4d75aa8237b15e
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-22 00:59:26 +0530

    Update print_colour_code.py

--- print_colour_code.py
+++ print_colour_code.py
@@ -1,11 +1,20 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-
-def print_table():
-    data = {'Major colour':['White', 'White', 'White', 'White','White','Red','Red','Red','Red','Red','Black','Black','Black','Black','Black', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Yellow','Violet','Violet','Violet','Violet','Violet'],
-        'Minor colour':["Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate"]}
- 
-    df = pandas.DataFrame(data)
-    return df
+# table = [['1', 2222, 30, 500], [4, 55, 6777, 1]]
+table=[['Pair number','Major colour','Minor colour']]
+pair_number=1
+l=[]
+for major_colour in MAJOR_COLORS:
+    for minor_colour in MINOR_COLORS:
+        l.append(pair_number)
+        l.append(major_colour)
+        l.append(minor_colour)
+        table.append(l)
+        pair_number+=1
+        l=[]
+print(table)
+for row in table:
+    print('--------------------------------------------')
+    print('| {:^11} | {:^12} | {:^12} |'.format(*row))
     
     
