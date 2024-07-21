740dfafa3473783f5e46fdc61ecadea691f30d70
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-21 23:35:26 +0530

    Update sub_functions.py

--- sub_functions.py
+++ sub_functions.py
@@ -12,12 +12,12 @@ def get_pair_number_from_color(major_color, minor_color):
     raise Exception('Minor index out of range')
   return major_index * len(MINOR_COLORS) + minor_index + 1
 
-  def get_color_from_pair_number(pair_number):
-    zero_based_pair_number = pair_number - 1
-    major_index = zero_based_pair_number // len(MINOR_COLORS)
-    if major_index >= len(MAJOR_COLORS):
-      raise Exception('Major index out of range')
-    minor_index = zero_based_pair_number % len(MINOR_COLORS)
-    if minor_index >= len(MINOR_COLORS):
-      raise Exception('Minor index out of range')
-    return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
+def get_color_from_pair_number(pair_number):
+  zero_based_pair_number = pair_number - 1
+  major_index = zero_based_pair_number // len(MINOR_COLORS)
+  if major_index >= len(MAJOR_COLORS):
+    raise Exception('Major index out of range')
+  minor_index = zero_based_pair_number % len(MINOR_COLORS)
+  if minor_index >= len(MINOR_COLORS):
+    raise Exception('Minor index out of range')
+  return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
