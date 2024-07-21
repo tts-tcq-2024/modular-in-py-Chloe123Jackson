1a09b89daf33f4b11952cdcd89eb8ff69b337886
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-22 01:02:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  print_colour_code()
+  print_colour_code_table()
   print('Done :)')
