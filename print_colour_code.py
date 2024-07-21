505c3af01bf848473d1d4cb21bc38b081bfa1776
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-22 00:40:28 +0530

    Update print_colour_code.py

--- print_colour_code.py
+++ print_colour_code.py
@@ -1,6 +1,3 @@
-#import pandas as pd
-from tabulate import tabulate
-
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
@@ -8,7 +5,7 @@ def print_table():
     data = {'Major colour':['White', 'White', 'White', 'White','White','Red','Red','Red','Red','Red','Black','Black','Black','Black','Black', 'Yellow', 'Yellow', 'Yellow', 'Yellow', 'Yellow','Violet','Violet','Violet','Violet','Violet'],
         'Minor colour':["Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate","Blue", "Orange", "Green", "Brown", "Slate"]}
  
-    df = 'f'
+    df = pandas.DataFrame(data)
     return df
     
     
