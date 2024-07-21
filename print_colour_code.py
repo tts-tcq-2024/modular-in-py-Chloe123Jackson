5d83051f6932a4e9f190ae1e3695a7e355cc0693
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-22 01:01:58 +0530

    Update print_colour_code.py

--- print_colour_code.py
+++ print_colour_code.py
@@ -1,20 +1,21 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-# table = [['1', 2222, 30, 500], [4, 55, 6777, 1]]
-table=[['Pair number','Major colour','Minor colour']]
-pair_number=1
-l=[]
-for major_colour in MAJOR_COLORS:
-    for minor_colour in MINOR_COLORS:
-        l.append(pair_number)
-        l.append(major_colour)
-        l.append(minor_colour)
-        table.append(l)
-        pair_number+=1
-        l=[]
-print(table)
-for row in table:
-    print('--------------------------------------------')
-    print('| {:^11} | {:^12} | {:^12} |'.format(*row))
+
+def print_colour_code_table():
+    table=[['Pair number','Major colour','Minor colour']]
+    pair_number=1
+    row=[]
+    for major_colour in MAJOR_COLORS:
+        for minor_colour in MINOR_COLORS:
+            l.append(pair_number)
+            l.append(major_colour)
+            l.append(minor_colour)
+            table.append(l)
+            pair_number+=1
+            l=[]
+            print(table)
+    for row in table:
+        print('--------------------------------------------')
+        print('| {:^11} | {:^12} | {:^12} |'.format(*row))
     
     
