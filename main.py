cda4c3518b23a582b4683adf44db26c7e538d7d0
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-21 23:32:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+# MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+# MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-import subfunctions.py
+import sub_functions import *
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
 
